dcb3d7c1ea398b6dae0678eb7540c5560f7e40ab
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-05-13 18:20:00 -0300

    Update Testes.cs

--- Tests/BDD/Testes.cs
+++ Tests/BDD/Testes.cs
@@ -8,6 +8,7 @@ namespace Tests.BDD
         public void ExecutarCenarios()
         {
             // Este teste é apenas um placeholder para permitir que o SpecFlow execute os cenários
+            
         }
     }
 }
