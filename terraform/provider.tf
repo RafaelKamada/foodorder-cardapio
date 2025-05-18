ac3738629b231c63511ebcf37565e8767913ff54
RafaelKamada <rafael-kamada2007@hotmail.com>
2025-05-18 19:22:25 -0300

    cardapio 9

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,6 @@ provider "aws" {
 
 provider "kubernetes" {
   host = var.host
-  cluster_ca_certificate = var.cluster_ca_certificate
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args = ["eks", "get-token", "--cluster-name", var.projectName]
